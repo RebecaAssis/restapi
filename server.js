4e1ea00d2e12bd83c370cfde3407b5c203c45c54
Rebeca Assis <rebeca.assis@brainboss.com.br>
2023-09-01 01:38:39 -0300

    feat: configured the DB connection using mongoose

--- server.js
+++ server.js
@@ -2,4 +2,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost/subscribers",
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    }
+);
+
+const db = mongoose.connection;
+db.on('error', (err) => console.log(err));
+db.once('open', () => console.log('Database connected!'));
+
+
+
+
 app.listen(3000, () => console.log('Server running'));
