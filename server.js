620f425d5f0720680b46c359768192f31ef22706
Rebeca Assis <rebeca.assis@brainboss.com.br>
2023-09-01 02:00:02 -0300

    feat: created the env. variables and cofigured the api to use ir and the json pattern

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// buscando a raíz do projeto o .env
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/subscribers",
+mongoose.connect(process.env.DATABASE_STRING,
     { 
         useNewUrlParser: true, 
         useUnifiedTopology: true 
@@ -13,7 +16,8 @@ const db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 db.once('open', () => console.log('Database connected!'));
 
-
+// Carregar/Aceitar o padrão json, pois vamos uilizar uma REST APi
+app.use(express.json());
 
 
 app.listen(3000, () => console.log('Server running'));
