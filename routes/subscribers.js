30f54437d5ab928f5e5ce04177b66def0ede7325
Rebeca Assis <rebeca.assis@brainboss.com.br>
2023-09-01 14:03:03 -0300

    feat: configured the data post and get on the API.

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,16 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const Subscriber = require('../models/subscriber');
 
-router.get('/', (req, res) => {
-    res.send('REST API ok!');
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find();
+        res.json(subscribers);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 router.get('/:id', (req, res) => {
 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+    const subscriber = new Subscriber({
+        userName: req.body.userName,
+        userChannel: req.body.userChannel,
+    });
 
+    try {
+        const newSubscriber = await subscriber.save();
+        res.status(201).json(newSubscriber);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
 });
 
 router.patch('/:id', (req, res) => {
